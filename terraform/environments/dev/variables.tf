6342d251b00ebd1336639763d618bbe1b622837c
diego <diegoyloayza@gmail.com>
2026-01-19 18:49:07 -0500

    Fixing errors.

--- terraform/environments/dev/variables.tf
+++ terraform/environments/dev/variables.tf
@@ -7,65 +7,54 @@ variable "aws_region" {
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "dev"
 }
 
 variable "project_name" {
   description = "Project name"
   type        = string
-  default     = "eks-catalyst"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR block"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
   description = "Private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.128.0/19", "10.0.160.0/19", "10.0.192.0/19"]
 }
 
 variable "public_subnets" {
   description = "Public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.0.0/20", "10.0.16.0/20", "10.0.32.0/20"]
 }
 
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.34"
 }
 
 variable "cluster_endpoint_public_access_cidrs" {
   description = "CIDR blocks that can access the public endpoint"
   type        = list(string)
-  default     = ["0.0.0.0/0"]
 }
 
 variable "vpc_cni_version" {
   description = "VPC CNI add-on version"
   type        = string
-  default     = "v1.15.1-eksbuild.1"
 }
 
 variable "coredns_version" {
   description = "CoreDNS add-on version"
   type        = string
-  default     = "v1.10.1-eksbuild.6"
 }
 
 variable "kube_proxy_version" {
   description = "kube-proxy add-on version"
   type        = string
-  default     = "v1.34.2-eksbuild.2"
 }
 
 variable "ebs_csi_driver_version" {
   description = "EBS CSI driver add-on version"
   type        = string
-  default     = "v1.25.0-eksbuild.1"
 }
